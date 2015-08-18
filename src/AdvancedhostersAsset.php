c0cdeb7dcce10eb4accfc9d1290121a0f0d93342
Andrii Vasyliev <sol@hiqdev.com>
2015-08-18 11:45:48 +0000

    php-cs-fixed

--- src/AdvancedhostersAsset.php
+++ src/AdvancedhostersAsset.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * Yii 2 asset with AdvancedHosters logos and so on
+ *
+ * @link      https://github.com/advancedhosters/yii2-asset-advancedhosters
+ * @package   yii2-asset-advancedhosters
+ * @license   No license
+ * @copyright Copyright (c) 2015, AdvancedHosters (https://advancedhosters.com/)
+ */
+
 namespace advancedhosters\assets\advancedhosters;
 
 class AdvancedhostersAsset extends \yii\web\AssetBundle
