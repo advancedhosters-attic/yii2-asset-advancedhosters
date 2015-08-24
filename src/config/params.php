da0efabe36f85dc5f9c68b5cdee77b6fd296eacd
Andrii Vasyliev <sol@hiqdev.com>
2015-08-24 08:58:00 +0000

    + config/params with AdvancedHosters general params

---
+++ src/config/params.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'orgName'      => 'AdvancedHosters',
+    'orgUrl'       => 'https://advancedhosters.com/',
+    'supportEmail' => 'support@advancedhosters.com',
+    'adminEmail'   => 'support@advancedhosters.com',
+];
