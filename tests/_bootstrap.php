438fa64e28747dd3126ede430cb89195c5887f29
Andrii Vasyliev <sol@hiqdev.com>
2016-06-04 14:50:40 +0300

    inited empty tests

---
+++ tests/_bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * AdvancedHosters asset files: images, configs and more
+ *
+ * @link      https://github.com/advancedhosters/yii2-asset-advancedhosters
+ * @package   yii2-asset-advancedhosters
+ * @license   proprietary
+ * @copyright Copyright (c) 2015-2016, AdvancedHosters (https://advancedhosters.com/)
+ */
+
+error_reporting(E_ALL & ~E_NOTICE);
+
+$bootstrap = __DIR__ . '/../src/_bootstrap.php';
+
+require_once file_exists($bootstrap) ? $bootstrap : __DIR__ . '/../vendor/autoload.php';
